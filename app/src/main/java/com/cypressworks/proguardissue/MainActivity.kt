89d0ecc6e6d30913f237eaca0a0dfa670aa6d7c3
Kirill Rakhman <kirill.rakhman@gmail.com>
2016-10-17 14:09:03 +0200

    dex bug

--- app/src/main/java/com/cypressworks/proguardissue/MainActivity.kt
+++ app/src/main/java/com/cypressworks/proguardissue/MainActivity.kt
@@ -2,15 +2,22 @@ package com.cypressworks.proguardissue
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity(), Mixin {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         foo()
 
-        doAsync {
-            1 + 1
-        }
+        listOf<Station>().forEachIndexed { i, (id, name, distance) -> println("${id} ${distance}") }
     }
 
 }
+
+data class GeoLocation(val lat: Double, val lng: Double)
+
+data class Station(
+        val id: String?,
+        val name: String,
+        val distance: Int,
+        val location: GeoLocation?) : Serializable
